c4cb39f9fd4d8911e9539290e8bc8128e6179f56
Jonathan Haines <jonno.haines@gmail.com>
2024-04-29 12:45:29 +1000

    updates

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ open "iterm/dracula/Dracula.itermcolors"
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 # Save iTerm preferences to dotfiles
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2/settings"
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/.config/iterm2/settings"
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
